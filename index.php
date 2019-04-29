ee5900f123cac852d9f21411573c42bd12572ee1
Margaret Thurston <mthursto@u.rochester.edu>
2019-04-29 11:07:33 -0400

    added content on homepage

--- index.php
+++ index.php
@@ -4,10 +4,48 @@
 		$title = "Index";
 		include "includes/head.php";?>
 	<body>
-		<?php include "includes/logo.php" ?>
-		<?php include "includes/nav.php" ?>
+		<section class="container">
+			<h1><?php include "includes/logo.php" ?></h1>
+			<?php include "includes/nav.php" ?>
 
-		<p>home</p>
+			<div id="home-top">
+				<h2>A United Voice</h2>
+				<div>Federation of Monroe County Environmentalists supports responsible growth and development within the Monroe County area. Full protection of our environment must not be compromised. </div>
+			</div>
+
+			<div>
+				<h3>The Federation...</h3>
+				<ul>
+					<li>facilitates communication among environmental groups.</li>
+					<li>provides a central forum for local organizations to discuss issues, prioritize actions, and enlist wider support.</li>
+					<li>presents positions to the public and to elected officials on issues vital to achieving a balance between environmental preservation and urban/suburban development.</li>
+				</ul>
+			</div>
+
+			<div>
+				<h3>Sustainability...</h3>
+				<p>Our present focus is on sustainable development. We are working to familiarize citizens and decision makers in our community with the principles of balanced, sustainable development, to show that preserving and protecting environmental integrity benefits every citizen, now and in the future.</p>
+			</div>
+
+			<div>
+				<h3>Sustainable Communities...</h3>
+				<ul>
+					<li>balance economic development with environmental protection, to provide ample support for productive human life.</li>
+					<li>require intergovernmental cooperation in planning and implementation, first on a county basis and then regionally.</li>
+					<li>focus upon activities which do not deplete or degrade those natural resources upon which present and future economic growth depend.</li>
+				</ul>
+			</div>
+
+			<div>
+				<h3>Membership...</h3>
+				<p>The Federation is open to all organizations which are committed to the principles of environmental protection. Each member organization should designate at least one representative to attend our monthly meetings. Individuals who are active in environmental causes, but do not represent any specific organization are equally welcome.</p>
+			</div>
+
+			<figure>
+				<img src="images/Slide1.gif" alt="slide1">
+			</figure>
+
+		</section>
 
 	</body>
 </html>
